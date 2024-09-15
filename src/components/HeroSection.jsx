a80087a7b1a2bbd07aee64fd03d8ee7a058ad77b
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2024-09-15 00:51:44 -0700

    small change

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ function HeroSection(){
     return(
         <div className="hero_section flex_row">
             <img src={heroImage} alt="" />
-            <HeroSectionCard productName={"Watch"} productDescription={"This is a watch with things on it. it likes penis and eats balls"}></HeroSectionCard>
+            <HeroSectionCard productName={"Watch"} productDescription={"This is a watch with things on it."}></HeroSectionCard>
         </div>
     )
 }
