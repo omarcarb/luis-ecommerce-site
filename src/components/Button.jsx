bcef36049b33dc0d15519a3bba4a9dbe31e9571f
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2024-12-01 13:10:14 -0800

    fixed issue of buttons not appearing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button({buttonText, buttonType}){
 
-    switch ({buttonType}){
+    switch (buttonType){
         case "primary":
             return(
                 <button className="primary_button">{buttonText}</button>
--- src/components/HeroSectionCard.jsx
+++ src/components/HeroSectionCard.jsx
@@ -7,8 +7,8 @@ function HeroSectionCard({productName, productDescription}){
             <h1>{productName}</h1>
             <p>{productDescription}</p>
             <div className="button_container">
-                <Button buttonText={"this is your mother"} buttonType="primary"></Button>
-                <Button buttonText={"This is another button"} buttonType="secondary"></Button>
+                <Button buttonText={"this is your mother"} buttonType="primary" />
+                <Button buttonText={"This is another button"} buttonType="secondary" />
             </div>
             
         </div>
