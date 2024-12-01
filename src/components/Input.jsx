81b8c90664f603ac7f84aca1a70f7fbf47ed06ee
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2024-11-30 22:48:49 -0800

    imported new fonts and more variables

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,13 +3,10 @@ import search from "./icons/i_search.svg"
 
 
 function Input({hasIcon}){
-    function MouseEnter(){
-    }
-
     return(
         <div className="input_container">
             {hasIcon && <img src={search} alt="" className="icon icon_input"/>}    
-            <input type="text" className="search" placeholder="Search" onMouseEnter={MouseEnter}/>
+            <input type="text" className="search" placeholder="Search"/>
         </div>
     )
 }
