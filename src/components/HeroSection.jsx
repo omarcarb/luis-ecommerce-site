0eb87d703b6d23f580a00226816d5ffe259ea405
Omar Carbajal <omarcarb04@gmail.com>
2024-12-04 14:51:00 -0800

    changes to product card

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,8 +5,8 @@ import heroImage from './images/hero_placeholder.webp'
 function HeroSection(){
     return(
         <div className="hero_section flex_row">
-            <img src={heroImage} alt="" />
             <HeroSectionCard productName={"Watch"} productDescription={"This is a watch with things on it."}></HeroSectionCard>
+            <img src={heroImage} alt="" />
         </div>
     )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function ProductCard({ product }){
     return (
         <div className="product-card">
-            <img src={product.image} alt="" />
-            <h3 className="card_name">{product.product_name}</h3>
-            <p className="card_price">Price: ${product.price}</p>
+            <img src={product.image} alt={product.description} />
+            <div className="card_header flex_row">
+                <h3 className="card_name">{product.product_name}</h3>
+                <p className="card_price">${product.price}</p>
+            </div>
             <p className="card_description">{product.description}</p>
         </div>
     );
