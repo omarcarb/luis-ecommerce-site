4c71048e30996b87398ddf890ad59affbe95126a
Omar Carbajal <omarcarb04@gmail.com>
2024-12-04 14:31:45 -0800

    changed icon element

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,10 @@ import search from "./icons/i_search.svg"
 function Input({hasIcon}){
     return(
         <div className="input_container">
-            {hasIcon && <img src={search} alt="" className="icon icon_input"/>}    
+            {hasIcon && <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="icon icon_input">
+                            <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" ></path>
+                        </svg>
+            }    
             <input type="text" className="search" placeholder="Search..."/>
         </div>
     )
