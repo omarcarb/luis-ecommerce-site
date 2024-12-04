9195c7b46e93b8603d9a1fbf23960b4a2bd7ca6d
Omar Carbajal <omarcarb04@gmail.com>
2024-12-03 17:57:10 -0800

    justified content to the left

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,7 +6,7 @@ function Input({hasIcon}){
     return(
         <div className="input_container">
             {hasIcon && <img src={search} alt="" className="icon icon_input"/>}    
-            <input type="text" className="search" placeholder="Search"/>
+            <input type="text" className="search" placeholder="Search..."/>
         </div>
     )
 }
