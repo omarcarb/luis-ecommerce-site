351ae408953443b0ae5b4119aa8cc437eb02f61a
Omar Carbajal <omarcarb04@gmail.com>
2024-12-10 15:35:41 -0800

    changed input group to flex_column

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,21 +4,21 @@ import Input from "./Input";
 function ContactForm(){
 
     return(
-        <section className="flex_row">
+        <section className="flex_row" id="contact_form_group">
             <div>
                 <h2>Want to ask a Question?</h2>
                 <p>Contact us about any inquiries about a product or service we provide.</p>
             </div>
-            <div className="input_form">
-                <div className="input_group">
+            <div className="input_form flex_column">
+                <div className="input_group flex_column">
                     <label htmlFor="fname_input">Full Name</label>
                     <Input hasIcon={false} placeholderText={"John Smith"} name='fname_input'></Input>
                 </div>
-                <div className="input_group">
+                <div className="input_group flex_column">
                     <label htmlFor="email">Email</label>
                     <Input hasIcon={false} placeholderText={"example@domain.com"} name='email'></Input>
                 </div>
-                <div className="input_group">
+                <div className="input_group flex_column">
                     <label htmlFor="messages">Message</label>
                     <Input hasIcon={false} placeholderText={'Type Here...'} name='messages' className={'message_box'}></Input>
                 </div>
